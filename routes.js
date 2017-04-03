75c5e10e2ac536b53ec097cc4a9915167c84ad57
Ayman Hamdy El-Toukhy <Toukhy@users.noreply.github.com>
2017-04-03 18:13:38 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,10 +3,6 @@ var router = express.Router();
 var eventController = require('./controllers/eventController');
 var scheduleController = require('./controllers/scheduleController');
 
-
-router.post('/event', eventController.createEvent);
-router.put('/event', eventController.updateEvent);
-router.post('/schedule', scheduleController.postSchedule);
 router.post('/deleteUser', deleteDiscussionUser.deleteDiscussion);
 router.post('/deleteSP', deleteDiscussionSer.deleteDiscussion);
 router.post('/deleteEvent', deleteEvent.deleteEvent);
