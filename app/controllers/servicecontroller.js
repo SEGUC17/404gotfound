16122c03136174420b4dca42b965444df4511371
omarmono1 <omar.elgendy@student.guc.edu.eg>
2017-04-05 16:50:53 +0200

    modified

--- app/controllers/servicecontroller.js
+++ app/controllers/servicecontroller.js
@@ -4,66 +4,54 @@ let serviceController = {
     
     login:function(req, res){
     
-        
-ServiceProvider.findOne({username:req.body.username}, function(err, doc) {
-    if (err) {
-        
-     res.send(err.message);
-
-
-    }
-else{
-if(req.body.password==doc.password){
-res.redirect('/serviceprovider');
-
-
-}
-
-
-
-}
-
-})},
-
-
-        
-
-    signup:function(req, res){
-        
-
-
-ServiceProvider.find({username:req.body.username},function(err,serviceprovider){
+ServiceProvider.findOne({username:req.body.username},function(err,servicelogin){
 if(err){
-let serviceprovider = new ServiceProvider(req.body);
-serviceprovider.name=req.body.name;
-serviceprovider.username=req.body.username;
-//serviceprovider.id=req.body.id; how the id will be passed,quto genrated from db
-serviceprovider.password=req.body.password;
-serviceprovider.category=req.body.category;
-serviceprovider.serviceoffered=req.body.serviceoffered;
-serviceprovider.schedule=req.body.schedule;
+res.send(err.message);
+}
+if(req.body.password==servicelogin.password){
+//res.redirect("/profile");
+console.log("logged in");
 
+res.json(servicelogin);
+}
+})},
+    
+    signup:function(req,res){
+ServiceProvider.findOne({username:req.body.username},function(err,servicrproviderreturn){
+if(err){
+    res.send(err.message);
+}
+if(servicrproviderreturn){
+res.json("already exists");
+
+
+}
+else{
+serviceprovider = new ServiceProvider(req.body);
 serviceprovider.save(function(err){
             if(err){
                 res.send(err.message)
                 console.log(err);
-            }
-            else{
-
-              //  console.log(serviceprovider);
-                res.redirect('/serviceprovider');
-            }
-        })
-}
-
-})
-
-
-
 
 
         
     }
+            else{
 
+              
+               // res.redirect('/serviceprovider');
+     res.json({ message: 'ServiceProvider added' });        
+      //  
+    }
+        })
+
+}
+
+
+
+})
+
+
+    }
 }
 module.exports = serviceController;
--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,9 @@ var serviceController = require('./controllers/servicecontroller');
 // add routes
 router.post('/signup', serviceController.signup);
 router.post('/login', serviceController.login);
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
 
 //router.post('/serviceprovider', projectController.createProject);
