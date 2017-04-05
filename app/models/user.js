dedc70270c15dc97a0d5c7760bd41d0def2e242f
MohamedNassefSelim <m_amr1996@hotmail.com>
2017-04-05 17:36:23 +0200

    add files

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require ('mongoose');
+
+var userSchema = mongoose.Schema({
+
+ Username: {
+   type:String,
+   required : true,
+   unique:true },
+
+   Password: {
+     type:String,
+     required : true,
+   },
+
+ name : String ,
+ age : Number,
+ weight : Number,
+ target_weight : Number,
+ height: Number,
+ BMI : Number,
+ body_fat: Number
+
+ });
+   var User = mongoose.model("user", userSchema);
+
+   module.exports = User;
