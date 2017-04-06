ecc100763aa77a97f2fb4610df0b8151630ae4f8
bassant97 <bassantashraf_97@hotmail.com>
2017-04-06 23:31:43 +0200

    Update deleteDiscussionSer.js

--- app/controllers/deleteDiscussionSer.js
+++ app/controllers/deleteDiscussionSer.js
@@ -5,12 +5,12 @@ let deleteDiscussionSer = {
   deleteDiscussion:function(req, res){
     Post.findById(req.body._id).remove().exec(function(err,deleted){
       if(err){
-        console.log("Error");
+        
         res.send(err.message);
 
       }
       else {
-        console.log("deleted");
+        
         res.send(deleted);
       }
 
