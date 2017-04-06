f02c27fda54f5f091a7ca048bfc77c4d0dd1b239
bassant97 <bassantashraf_97@hotmail.com>
2017-04-07 00:05:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var DB_URI = "mongodb://localhost:27017/portfolio";
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname , 'views'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
