14f7fd50d4e5ae91c00f3d376083d18aab49db18
bassant97 <bassantashraf_97@hotmail.com>
2017-04-06 23:33:35 +0200

    Update deleteSchedule.js

--- app/controllers/deleteSchedule.js
+++ app/controllers/deleteSchedule.js
@@ -5,12 +5,12 @@ let deleteSchedule = {
   deleteSchedule:function(req, res){
     Schedule.findById(id =  req.body._id).remove().exec(function(err,deleted){
       if(err){
-        console.log("Error");
+        
         res.send(err.message);
 
       }
       else {
-        console.log("deleted");
+        
         res.send(deleted);
       }
 
