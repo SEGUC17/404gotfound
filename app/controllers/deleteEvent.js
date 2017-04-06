5da43c6668e3959871c8b4cc81701bdd7e0695c8
bassant97 <bassantashraf_97@hotmail.com>
2017-04-06 23:32:53 +0200

    Update deleteEvent.js

--- app/controllers/deleteEvent.js
+++ app/controllers/deleteEvent.js
@@ -4,12 +4,12 @@ let deleteEvent = {
   deleteEvent:function(req, res){
     Event.findById(req.body._id).remove().exec(function(err,deleted){
       if(err){
-        console.log("Error");
+       
         res.send(err.message);
 
       }
       else {
-        console.log("deleted");
+        
         res.send(deleted);
       }
 
