353895e81593d8e4c8b96bd17c8fc838c78a9d97
bassant97 <bassantashraf_97@hotmail.com>
2017-04-06 23:32:19 +0200

    Update deleteDiscussionUser.js

--- app/controllers/deleteDiscussionUser.js
+++ app/controllers/deleteDiscussionUser.js
@@ -5,12 +5,12 @@ let deleteDiscussionUser = {
   deleteDiscussion:function(req, res){
     Discussion.findById(req.body._id).remove().exec(function(err,deleted){
       if(err){
-        console.log("Error");
+        
         res.send(err.message);
 
       }
       else {
-        console.log("deleted");
+        
         res.send(deleted);
       }
 
